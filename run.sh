2c9bc20924f31df04bd3400f8400eb63333879e4
sermepratik <pratikghatpande0@gmail.com>
2015-07-10 21:58:07 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/unique_words.txt
+python ./src/wordsintweets.py ./tweet_input/tweets.txt ./tweet_output/unique_words.txt
+
+python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/running_median.txt
